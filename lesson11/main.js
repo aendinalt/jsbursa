f35d2a31300e36dd057c08090792fc91f45a4998
Gessika <lordlemale@gmail.com>
2014-11-12 11:10:11 +0200

    task 11 temp save

--- lesson11/main.js
+++ lesson11/main.js
@@ -20,15 +20,16 @@ angular.module('task12').factory('loginService', ['transportService', function (
             // получить от сервера seed
             var objToSend = {login: username, password: password};
 
-            var seed =  transportService.send(objToSend).then(function(message){
-                console.log(message.seed);
+            transportService.send(objToSend).then(function(message){
                 return message.seed;
-            });
+            })
+                .then(function(seed){
+                    objToSend = {seed: seed + superSecretKey};
+                    transportService.send(objToSend).then(function(message){
+                        console.log(message);
+                    })
+                });
 
-
-            return transportService.send({seed: seed+superSecretKey}).then(function(message){
-               console.log(message);
-            });
 
             // отправить на сервер {seed: seed+superSecretKey}
             // получить от сервера authLogin
@@ -44,18 +45,31 @@ angular.module('task12').factory('transportService', ['$q', function ($q) {
     function getConnectedWebSocket() {
         //Здесь разрешено писать ваш код
         var deferred = $q.defer();
-        if (!ws){
-            ws = new WebSocket(URL);
-        }
+        ws = new WebSocket(URL);
 
         ws.onopen = function(){
             deferred.resolve(ws);
         };
 
-        ws.onclose =  function(){
+        ws.onclose = (function(){
+            console.log('Канал закрыт!');
             ws = new WebSocket(URL);
+            ws.onopen = function(){
+                console.log('открыли новый канал');
+                deferred.resolve(ws);
+
+            };
+
+        });
+
+        ws.onerror = (function(){
+            alert('Ошибка!');
+
+        });
+
+        if (ws.readyState === ws.OPENED){
             deferred.resolve(ws);
-        };
+        }
 
         return deferred.promise;
 
@@ -67,7 +81,10 @@ angular.module('task12').factory('transportService', ['$q', function ($q) {
         getConnectedWebSocket().then(function (socket) {
             //Здесь разрешено писать ваш код
             socket.send(message);
+            console.log('отправлено  сообщение');
+            console.log(message);
             socket.onmessage = function(message){
+                console.log('получено  сообщение');
                 console.log(message);
                 var data = JSON.parse(message.data);
                 deferred.resolve(data);
